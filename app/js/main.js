fc263c1b76b70093087282c5dea470bf5ceb01f4
Misha <mirzoevmisha7@gmail.com>
2020-04-16 12:10:21 +0300

    block tabs is made

--- app/js/main.js
+++ app/js/main.js
@@ -9,7 +9,21 @@ $(document).ready(function() {
 		$(".js-tab-content.active").removeClass("active");
 		content.addClass("active");
 	});
+
+	$(".js-tab-header").click(function(){
+		let id = $(this).attr('data-tab'),
+		content = $('.js-tab-body[data-tab="'+ id +'"]');
+
+		$(".js-tab-header.active").removeClass("active");
+		$(this).addClass("active");
+
+		$(".js-tab-body.active").removeClass("active");
+		content.addClass("active");
+	});
+
 });
+
+
 
 
 
